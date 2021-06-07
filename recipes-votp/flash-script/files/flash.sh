8c63f59bf59318e646eb178c4cfdd9007f7d834e
Albert Babí <albert.babi@savoirfairelinux.com>
2021-06-07 12:33:55 +0200

    recipes-votp/flash-script: update script to flash EFI images
    - Add bmaptool support for flashing
    - Create EFI entries for corresponding images
    - Set top boot order priority for USB and PXE entries
    
    Issue: #5911
    Signed-off-by: Albert Babí <albert.babi@savoirfairelinux.com>
    Change-Id: Ib7a3e3213785debc28c1d6916dfae5215bc06e2a

--- recipes-votp/flash-script/files/flash.sh
+++ recipes-votp/flash-script/files/flash.sh
@@ -5,7 +5,7 @@
 usage()
 {
     echo 'Flash an image on disk'
-    echo 'usage: flash [-h] [-i] image -o disk'
+    echo 'usage: flash [-h] [-i] image -d disk'
     echo
     echo 'mandatory arguments:'
     echo '  -i,--image   image  the image wic or wic.gz image to flash'
@@ -72,18 +72,72 @@ if [ ! -f "${image}" ] ; then
     exit 1
 fi
 
-if echo "$image" | grep -qE "\.gz$"  ; then
-    command="gzip -d -c \"$image\" | dd of=\"${disk}\" bs=32M"
+bmap="${image%.*}.bmap"
+if [ -f "${bmap}" ] ; then
+    command="bmaptool copy \"$image\" \"${disk}\""
 else
-    command="dd if=\"$image\" of=\"${disk}\" bs=32M"
+    if echo "$image" | grep -qE "\.gz$"  ; then
+        command="gzip -d -c \"$image\" | dd of=\"${disk}\" bs=32M"
+    else
+        command="dd if=\"$image\" of=\"${disk}\" bs=32M"
+    fi
 fi
 
 echo "$command"
 if eval "$command" ; then
     sync
     echo "Flash $image on $disk: success"
-    echo "You can reboot. Don't forget to remove your USB key."
 else
     echo "Flash $image on $disk: failed"
     exit 1
 fi
+
+# If EFI image create boot entries
+if command -v efibootmgr &> /dev/null ; then
+    echo "EFI image. Create boot entries"
+    if ! efibootmgr | grep -q "boot0" ; then
+        command="efibootmgr --create --disk \"$disk\" --part 1 --label \"boot0\" --loader /EFI/BOOT/bootx64.efi"
+        if eval "$command" ; then
+            echo "Entry boot0 sucessfully created"
+        else
+            echo "Error while creating entry boot0"
+            exit 1
+        fi
+    fi
+    if ! efibootmgr | grep -q "boot1" ; then
+        command="efibootmgr --create --disk \"$disk\" --part 2 --label \"boot1\" --loader /EFI/BOOT/bootx64.efi"
+        if eval "$command" ; then
+            echo "Entry boot1 sucessfully created"
+        else
+            echo "Error while creating entry boot1"
+            exit 1
+        fi
+    fi
+
+    # Set top boot order priority for USB and PXE entries
+    boot_order=$(efibootmgr | grep "BootOrder" | awk '{ print $2}')
+    usb_entries=$(efibootmgr | awk '/USB/{ gsub("Boot",""); gsub("*", ""); print $1 }')
+    pxe_entries=$(efibootmgr | awk '/PCI LAN/{ gsub("Boot",""); gsub("*", ""); print $1 }')
+
+    # Create top priority list and remove from original boot order
+    top_priority=""
+    for entry in $usb_entries $pxe_entries
+    do
+        top_priority+=$entry","
+        boot_order=${boot_order/$entry/}
+    done
+
+    # Concatenate lists and squeeze repeated commas
+    boot_order=$(echo $top_priority$boot_order | tr -s ",")
+
+    # Change boot order
+    command="efibootmgr -o $boot_order"
+    if eval "$command" ; then
+        echo "Boot order successfully changed"
+    else
+        echo "Error while changing boot order"
+        exit 1
+    fi
+fi
+
+echo "You can reboot. Don't forget to remove your USB key."
