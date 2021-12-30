0b8330e1b6bb4fff75e2abc242da7a9a0b2363c9
insatomcat <florent.carli@rte-france.com>
2021-12-30 19:00:06 +0100

    Add snmp capabilities to seapath
    This changes allow the seapath nodes to be monitored via snmp.
    First notice we have to bump net-snmp to version 5.9.1 (meta-networking,
    honister branch) in order to solve a bug version 5.8 has on hosts with
    ipv6 disabled (https://access.redhat.com/solutions/4213681).
    Another package (net-snmp-votp) has been created to host the
    configuration specificities.
    
    Signed-off-by: insatomcat <florent.carli@rte-france.com>

---
+++ recipes-networking/net-snmp-votp/files/snmp_dommemstat.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FILE=/tmp/dommemstat.txt
+
+function create_or_rewrite {
+  /usr/bin/virsh list --name | sed -s "/^$/d" | while read i
+  do
+    echo Domain: \'$i\'
+    /usr/bin/virsh dommemstat --domain $i
+  done > $FILE
+}
+
+if [ -f $FILE ]
+then
+  OLDTIME=60
+  CURTIME=$(date +%s)
+  FILETIME=$(stat $FILE -c %Y)
+  TIMEDIFF=$(expr $CURTIME - $FILETIME)
+
+  if [ $TIMEDIFF -gt $OLDTIME ]; then
+    create_or_rewrite
+  fi
+else
+  create_or_rewrite
+fi
+cat $FILE
