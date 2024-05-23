8e4472d21ece8454f9dfc21fe2c7ee92c55524c2
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2024-05-23 11:54:38 +0200

    system-upgrade: fix error handling and partition detection
    The script switch_bootloader.sh was not handling errors properly and
    was not able to detect the partition where the bootloader is installed
    when the device name was not /dev/sdX.
    
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

--- recipes-seapath/system-upgrade/files/switch_bootloader.sh
+++ recipes-seapath/system-upgrade/files/switch_bootloader.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Copyright (C) 2021, RTE (http://www.rte-france.com)
+# Copyright (C) 2024 Savoir-faire Linux, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Script that switches between bootloader "SEAPATH slot 0" and "SEAPATH slot 1"
 # EFI bootloader entries or disable the passive slot if the parameter disable is
 # given
 # Note: entries should already be created and boot order correctly
+
+set -e
 
 die()
 {
@@ -23,12 +26,24 @@ boot0=$(efibootmgr | \
 boot1=$(efibootmgr | \
     awk '/SEAPATH slot 1/{ gsub("Boot", ""); gsub("*", ""); print $1 }')
 
-[ -n "${boot0}" ] || die "Could not retrieve boot0 entry in EFI boot order"
+if [ ! -n "${boot0}" ] ; then
+    die "Could not retrieve boot0 entry in EFI boot order"
+fi
 if [ -z "${boot1}" ] ;then
     echo "Recreate SEAPATH slot 1 entry"
     upgradable_bootloader=$(readlink -f /dev/upgradable_bootloader)
-    disk=${upgradable_bootloader:0:-1}
-    efibootmgr -q -c -d "$disk" -p 2 -L "SEAPATH slot 1" -l /EFI/BOOT/bootx64.efi
+    if [ -z "${upgradable_bootloader}" ] ; then
+        die "Could not find upgradable bootloader"
+    fi
+    disk="/dev/$(lsblk -no pkname ${upgradable_bootloader})"
+    if ! efibootmgr \
+        -q -c -d "$disk" \
+        -p 2 \
+        -L "SEAPATH slot 1" \
+        -l /EFI/BOOT/bootx64.efi
+    then
+        die "Error while creating SEAPATH slot 1 entry"
+    fi
     exit 0
 fi
 
@@ -70,17 +85,21 @@ else
 fi
 
 if [ "$1" = "disable" ] ; then
-    efibootmgr -q -b "${passive_boot}" -A || die "Error while disabling entry" \
-        " ${passive_boot}"
+    if ! efibootmgr -q -b "${passive_boot}" -A ; then
+        die "Error while disabling entry ${passive_boot}"
+    fi
 else
     # Switch bootorder
     newbootorder=$(echo "${bootorder}" | \
         sed "s/${passive_boot}/${active_boot}/" | \
         sed "s/${active_boot}/${passive_boot}/")
     echo "Switch active and passive slot"
-    efibootmgr -q -o "${newbootorder}" || "Error could not switch the bootorder"
+    if ! efibootmgr -q -o "${newbootorder}" ; then
+        die "Error could not switch the bootorder"
+    fi
 
     echo "Enable the passive slot"
-    efibootmgr -q -b "${passive_boot}" -a || die "Error while disabling entry" \
-        " ${passive_boot}"
+    if ! efibootmgr -q -b "${passive_boot}" -a ; then
+        die "Error while disabling entry ${passive_boot}"
+    fi
 fi
